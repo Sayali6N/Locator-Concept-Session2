187050519b694112c373aa39c1d0ef80a3c5aac1
Sayali6N <sayalin06@gmail.com>
2022-01-14 15:13:23 -0600

    3rd commit

---
+++ src/test/java/git/TestingGitAgain.java
@@ -0,0 +1,12 @@
+package git;
+
+public class TestingGitAgain {
+
+	public static void main(String[] args) {
+		System.out.println("Checking Git newBranch");
+		System.out.println("Code to Creat & Switch between Branches is below");
+		System.out.println("git checkout -b demoNewBranch");
+
+	}
+
+}
