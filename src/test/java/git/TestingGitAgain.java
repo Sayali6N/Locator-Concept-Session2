f80d2f62578e9a06222b90967ce7196e014d9ff1
Sayali6N <sayalin06@gmail.com>
2022-01-14 15:24:58 -0600

    This is 4th commit

--- src/test/java/git/TestingGitAgain.java
+++ src/test/java/git/TestingGitAgain.java
@@ -6,7 +6,13 @@ public class TestingGitAgain {
 		System.out.println("Checking Git newBranch");
 		System.out.println("Code to Creat & Switch between Branches is below");
 		System.out.println("git checkout -b demoNewBranch");
-
+		System.out.println("Made some new changes");
+		System.out.println("To check git hub codes one more time");
+		System.out.println("Lets see it on git");
+		System.out.println("Command is git status ");
+		System.out.println("git add . enter ");
+		
 	}
+
 
 }
